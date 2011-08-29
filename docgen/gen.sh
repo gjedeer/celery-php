df71823f4d3b6d80a7d6a57284ac652345778c75
GDR\! <gdr@go2.pl>
2011-08-29 17:23:12 +0200

    Logo added

--- docgen/gen.sh
+++ docgen/gen.sh
@@ -4,3 +4,4 @@ cd /home/gdr/celery-php
 git pull
 
 phpdoc --title "PHP client for Celery task queue" -o HTML:Smarty:HandS -f /home/gdr/celery-php/celery.php -t /srv/celery-php-doc --sourcecode on
+cp docgen/logo.png /srv/celery-php-doc/media/
