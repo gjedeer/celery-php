723972fccb742f4afeeb9bd05a7bbae98eb2c0bc
GDR\! <gdr@gdr.name>
2013-05-09 17:36:53 +0000

    Updated phpunit install instruction in comment

--- unittest.php
+++ unittest.php
@@ -37,11 +37,14 @@
 
 /*
  * INSTALL:
-pear upgrade
-pear channel-discover pear.phpunit.de
-pear channel-discover components.ez.no
-pear channel-discover pear.symfony-project.com
-pear install --alldeps phpunit/PHPUnit
+sudo apt-get remove phpunit
+sudo pear channel-discover pear.phpunit.de
+sudo pear channel-discover pear.symfony-project.com
+sudo pear channel-discover components.ez.no
+sudo pear update-channels
+sudo pear upgrade-all
+sudo pear install --alldeps phpunit/PHPUnit
+sudo pear install --force --alldeps phpunit/PHPUnit
 
  * RUN:
 phpunit CeleryTest unittest.php
