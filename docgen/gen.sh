a00bcef8cacd7a6642e4ea48c9d1e3c898884842
GDR\! <gdr@gdr.name>
2016-08-10 12:12:24 +0000

    Use php5 for documenting

--- docgen/gen.sh
+++ docgen/gen.sh
@@ -3,5 +3,5 @@
 cd /home/gdr/celery-php
 git pull
 
-phpdoc --title "PHP client for Celery task queue" -o HTML:Smarty:HandS -f /home/gdr/celery-php/celery.php -t /srv/celery-php-doc --sourcecode on
+php5 /usr/bin/phpdoc --title "PHP client for Celery task queue" -o HTML:Smarty:HandS -f '/home/gdr/celery-php/*.php' -t /srv/celery-php-doc --sourcecode on
 cp docgen/logo.png /srv/celery-php-doc/media/
