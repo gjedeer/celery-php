bc196e0d93076d5356656ebfef6a2a31903e693d
Jon Dufresne <jon.dufresne@gmail.com>
2017-04-19 20:10:35 -0700

    Upgrade Celery config to new lower case settings
    http://docs.celeryproject.org/en/latest/userguide/configuration.html#new-lowercase-settings

--- testscenario/celeryamqplibconfig.py
+++ testscenario/celeryamqplibconfig.py
@@ -1,8 +1,4 @@
-BROKER_URL = "amqp://gdr:test@localhost:5672/celeryamqplib"
-
-CELERY_RESULT_BACKEND = "amqp"
-
-CELERY_IMPORTS = ("tasks", )
-
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_TASK_RESULT_EXPIRES = None
+imports = ['tasks']
+broker_url = 'amqp://gdr:test@localhost:5672/celeryamqplib'
+result_backend = 'amqp'
+result_expires = None
