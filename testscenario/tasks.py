a9fb057f0751beefdfc9793b327235100113bf05
Jon Dufresne <jon.dufresne@gmail.com>
2017-04-22 18:03:19 -0700

    Make testscenario Python3 compatible

--- testscenario/tasks.py
+++ testscenario/tasks.py
@@ -1,31 +1,35 @@
-#!/usr/bin/python
 from celery.task import task
 import time
 
+
 @task
 def add(x, y):
-    print "Got add()"
+    print("Got add()")
     return x + y
+
 
 @task
 def add_delayed(x, y):
-    print "Got add_delayed()"
+    print("Got add_delayed()")
     time.sleep(1)
-    print "Woke up from add_delayed()"
+    print("Woke up from add_delayed()")
     return x+y
+
 
 @task
 def fail():
-    print "Got fail()"
-    return fffffff
+    print("Got fail()")
+    raise Exception
+
 
 @task
 def delayed():
-    print "Got delayed()"
+    print("Got delayed()")
     time.sleep(2)
-    print "Woke up from delayed()"
+    print("Woke up from delayed()")
     return 'Woke up'
+
 
 @task
 def get_fibonacci():
-    return [1,1,2,3,5,8,13,21,34,55,89,144]
+    return [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
